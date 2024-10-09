8b575d6f4084ab1f9a91c3faf665e9422408eb03
Jakub-Chojna <jakub.chojna@gmail.com>
2024-10-09 18:40:58 +0200

    style: improve styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -14,10 +16,19 @@ const queryClient = new QueryClient({
   },
 });
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
