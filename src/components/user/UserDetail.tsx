f2b70c6b80c778723ebab8f33ca9b1cdec8dc014
Jakub-Chojna <jakub.chojna@gmail.com>
2024-10-10 11:39:14 +0200

    test: add tests for UserDetail

--- src/components/user/UserDetail.tsx
+++ src/components/user/UserDetail.tsx
@@ -8,9 +8,9 @@ type UserDetailProps = {
 const UserDetail = ({ detail, label }: UserDetailProps) => {
   return (
     <Stack direction="row" spacing="10px" sx={{ alignItems: "baseline" }}>
-      <Typography variant="body2">{`${label}:`}</Typography>
+      <Typography variant="body2">{label}</Typography>
       <Typography variant="body2" sx={{ fontWeight: 600 }}>
-        {detail ?? "-"}
+        {detail || "-"}
       </Typography>
     </Stack>
   );
